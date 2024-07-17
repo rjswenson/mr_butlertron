32e35e0c6b0a399259be0009a30597a4151cb84f
Robin Healey <rjswenson@gmail.com>
2024-07-17 17:10:01 -0500

    rejigger model to run against stdout for early testing

--- run_model.py
+++ run_model.py
@@ -1,18 +1,33 @@
-from transformers import pipeline
-from flask import Flask, request, jsonify
+#!/user/bin/env python3
 
-app = Flask(__name__)
+import sys
+# from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, GenerationConfig
+# from flask import Flask, request, jsonify
+
+# app = Flask(__name__)
 
 # Load a pre-trained model
-model_name = "distilbert-base-uncased"
-nlp = pipeline("sentiment-analysis", model=model_name)
+model_name = 'google/flan-t5-xl'
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+config = GenerationConfig(max_new_tokens=200)
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    data = request.json
-    text = data.get("text", "")
-    result = nlp(text)
-    return jsonify(result)
+# TODO: pipeline vs above model def?
+# nlp = pipeline("sentiment-analysis", model=model_name)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# creates a pytorch tensor
+for line in sys.stdin:
+    tokens = tokenizer(query_line, return_tensors="pytorch")
+    outputs = model.generate(**tokens, generation_config=config)
+    print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
+
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     data = request.json
+#     text = data.get("text", "")
+#     result = nlp(text)
+#     return jsonify(result)
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
